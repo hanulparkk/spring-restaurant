779c57deb09d1b1a3f307c380f7008a06d7c4344
hanulparkk <hanulparkk@gmail.com>
2021-02-20 23:44:53 +0900

    use webpack and enable cors

---
+++ eatgo-web/src/index.js
@@ -0,0 +1,20 @@
+(async () => {
+    const url = "http://localhost:8080/restaurants";
+    const response = await fetch(url);
+    const restaurants = await response.json();
+
+    const element = document.getElementById("app");
+    element.innerHTML = `
+        ${restaurants.map(restaurant => `
+            <p>
+                ${restaurant.id}
+                ${restaurant.name}
+                ${restaurant.address}
+            </p>
+        `).join('')}
+    `;
+    // JSON.stringify(restaurants);
+    // ${restaurants[0].id}
+    // ${restaurants[0].name}
+    // ${restaurants[0].address}
+})();
